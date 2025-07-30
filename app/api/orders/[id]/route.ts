e07b762852eb479ee7575e8e9d873a2c1f3b4abb
Fayiz <fayiz.islam@gmail.com>
2025-07-30 18:54:25 +0600

    Initial commit

--- app/api/orders/[id]/route.ts
+++ app/api/orders/[id]/route.ts
@@ -1,28 +1,25 @@
-import { NextRequest, NextResponse } from 'next/server';
 import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
 import { authOptions } from '@/lib/auth';
+import { NextResponse } from 'next/server';
 
-type Context = {
-  params: {
-    id: string;
-  };
-};
-
-export async function PUT(request: NextRequest, context: Context) {
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
   const session = await getServerSession(authOptions);
 
   if (!session || (session.user.role !== 'ADMIN' && session.user.role !== 'SUPERADMIN')) {
     return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   }
 
-  const { id } = context.params;
+  const { id } = params;
 
   try {
     const body = await request.json();
-    const { sentAt } = body;
 
     const existingOrder = await prisma.order.findUnique({
+      
       where: { id },
     });
 
@@ -32,14 +29,13 @@ export async function PUT(request: NextRequest, context: Context) {
 
     const isNowSent = !existingOrder.isSent;
 
-const updatedOrder = await prisma.order.update({
-  where: { id },
-  data: {
-    isSent: isNowSent,
-    sentAt: isNowSent ? new Date() : null, 
-  },
-});
-
+    const updatedOrder = await prisma.order.update({
+      where: { id },
+      data: {
+        isSent: isNowSent,
+        sentAt: isNowSent ? new Date() : null,
+      },
+    });
 
     return NextResponse.json(updatedOrder);
   } catch (error) {
